5ea4dfe9303979b66ab8cfa7743ae45ed810e5b9
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-07-26 00:49:12 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,16 +7,14 @@ class SchoolClassesController < ApplicationController
     def create
         @school_class = SchoolClass.create(school_classes_params(:title, :room_number))
         @school_class.save
-
         redirect_to school_class_path(@school_class)
     end
 
-    def edit
+    def show
         @school_class = SchoolClass.find(params[:id])
-
     end
 
-    def show
+    def edit
         @school_class = SchoolClass.find(params[:id])
     end
 
@@ -31,4 +29,4 @@ class SchoolClassesController < ApplicationController
     def school_classes_params(*args)
         params.require(:school_class).permit(*args)
     end
-end 
+end
